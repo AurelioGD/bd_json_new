5deae49359648837220ae70413dd9bf908181117
Aurelio Gonzalez <agg156231@gmail.com>
2021-12-21 11:58:17 -0700

    Instalando dependencia para generar id del doc

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 const { createCollection, createDatabase, addDoc } = require("./querys")
 const main = () => {
-    addDoc("xd", "pan", {
-        id: 1,
-        name: "concha"
-    })
+
 }
 main()
